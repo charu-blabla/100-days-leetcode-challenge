ec097678eb8bec8bf6877a98c635d5c5a2da6386
charu-blabla <malhotracharu56@gmail.com>
2026-07-18 14:47:16 +0530

    Refactor Fibonacci function for better performance
    Refactored Fibonacci function to improve efficiency using iteration instead of recursion.

--- Day-03/fib.py
+++ Day-03/fib.py
@@ -1,3 +1,4 @@
+## OLD METHOD OR ROOKIE VERSION : 
 class Solution:
     def fib(self, n: int) -> int:
 
@@ -5,3 +6,23 @@ class Solution:
             return n
         else : 
             return self.fib(n-1)+self.fib(n-2)
+
+
+## NEW AND UPDATED VERSION : 
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0:
+            return 0
+        if n==1 or n==2 :
+            return 1
+        else:
+            a,b=0,1
+            for i in range (2,n+1):
+                c=a+b
+                a,b=b,c
+            return b
+        
