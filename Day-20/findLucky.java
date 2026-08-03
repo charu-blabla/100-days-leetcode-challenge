33504ca29ac5de86b1aa8469d8118f58ff544810
charu-blabla <malhotracharu56@gmail.com>
2026-08-03 10:28:10 +0530

    Add findLucky method in Solution class
    Implement findLucky method to identify lucky integer.

---
+++ Day-20/findLucky.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findLucky(int[] arr) {
+
+       
+        int freq[] = new int[501];
+        for(int i : arr){
+           
+                freq[i]++;
+            
+        }
+        int max = -1;
+      
+        for(int i = 1;i<=500;i++){
+            if(freq[i]==i)
+                max = i;
+            
+        }
+        return(max);
+        
+    }
+}
